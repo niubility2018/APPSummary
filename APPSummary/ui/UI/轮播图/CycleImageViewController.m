2822df8961debbba17fe4dd2264122ac3f40d433
mystyle <xubojoy1112@163.com>
2018-07-06 15:05:07 +0800

    添加轮播图分类

---
+++ APPSummary/ui/UI/轮播图/CycleImageViewController.m
@@ -0,0 +1,115 @@
+//
+//  CycleImageViewController.m
+//  APPSummary
+//
+//  Created by xubojoy on 2018/7/6.
+//  Copyright © 2018年 xubojoy. All rights reserved.
+//
+
+#import "CycleImageViewController.h"
+#import "CycleStyleOneController.h"
+@interface CycleImageViewController ()<UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *dataArray;
+
+
+@end
+
+@implementation CycleImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.dataArray = @[@"样式一",@"样式二"];
+    [self initTableView];
+}
+
+- (void)initTableView{
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0,0, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStyleGrouped];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.tableView.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [UITableViewCell new];
+    
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    cell.textLabel.font = [UIFont systemFontOfSize:font_15_size];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 0;
+}
+//
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
+    return nil;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    switch (indexPath.row) {
+        case 0:
+        {
+            CycleStyleOneController *lanuvc = [[CycleStyleOneController alloc] init];
+            
+            [self.navigationController pushViewController:lanuvc animated:YES];
+        }
+            break;
+        case 1:
+        {
+            CycleStyleOneController *lanuvc = [[CycleStyleOneController alloc] init];
+            [self.navigationController pushViewController:lanuvc animated:YES];
+        }
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ APPSummary/ui/UI/轮播图/CycleStyleOneController.h
@@ -0,0 +1,13 @@
+//
+//  CycleStyleOneController.h
+//  APPSummary
+//
+//  Created by xubojoy on 2018/7/6.
+//  Copyright © 2018年 xubojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CycleStyleOneController : UIViewController
+
+@end
